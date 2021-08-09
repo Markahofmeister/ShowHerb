1c91f943eeb06456a67988b9ca637a82309afb54
markahofmeister <markhof12@gmail.com>
2021-08-09 11:52:15 -0400

    Added rain sensor testing code

---
+++ software/rainSensor-testing/rainSensor-testing.ino
@@ -0,0 +1,29 @@
+/* 
+ * Date: 08/09/2021
+ * 
+ * Rain sensor testing code 
+ * 
+ */
+
+ uint8_t digInputPin = 2;
+ uint8_t anInputPin = A0; 
+ uint8_t digInput;
+ uint16_t anInput; 
+void setup() {
+  
+   Serial.begin(9600);
+   pinMode(digInputPin, INPUT);
+   
+}
+
+void loop() {
+
+  digInput = digitalRead(digInputPin);
+  anInput = analogRead(anInputPin);
+  
+  Serial.print("Digital Output: "); Serial.println(digInput);
+  Serial.print("Analog Output: "); Serial.println(anInput);
+
+  delay(1000); 
+
+}
